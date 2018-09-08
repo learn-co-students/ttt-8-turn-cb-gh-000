248832da92219c2a4ce24aa7107f1e36e2097dda
Lukas F <lukas.forst48@googlemail.com>
2018-09-08 07:50:38 +0000

    Done.

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,48 +1,41 @@
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------\n"
+  puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------\n"
+  puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-#transform input to int
-def input_to_index(input)
-  return input.to_i - 1
+
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
 
-def move(board,position,move="X")
-  board[position] = move
-  return board;
-end
-# code your #valid_move? method here
-def valid_move?(board,index)
-  if(index.between?(0,8)&& !position_taken?(board,index))
-    return true
-  else
-    return false
+def valid_move?(board, index)
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
   end
-end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board,index)
-  if(board[index]==" "||board[index]==""||board[index]==nil)
-    return false
-  else
-    return true
-  end
+  # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input=gets
-  new_input=input_to_index(input)
-  if(new_input<9&&new_input>=0)
-    check=valid_move?(board,new_input)
-    if(check)
-      display_board(move(board,new_input))
-    end
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
   else
     turn(board)
   end
+end
+
+def position_taken?(board, index)
+  board[index] != " "
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
 end
