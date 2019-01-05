bd87ad96fb78ee1a0f24404b0991902ab6d95733
Naruethai Thongphasook <naruethai.th@student.chula.ac.th>
2019-01-05 20:40:38 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,40 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board,index)
+  if index >= 0 && index <=8
+    if board[index] == "X" || board[index] == "O"
+      return false
+    else
+      return true
+    end
+  else
+    return false
+  end
+end
+
+def input_to_index(n)
+ n = n.to_i
+ return n-1
+end
+
+def move(board,index,player="X")
+ board[index] = player
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board,index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
