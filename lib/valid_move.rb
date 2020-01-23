85c1e13ed08514a808791e5eaea9c803513b875c
Fouad Ahsayni <f.ahsayni@pm.me>
2020-01-23 13:39:06 +0000

    Done.

---
+++ lib/valid_move.rb
@@ -0,0 +1,23 @@
+def valid_move?(board, index)
+
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    return true
+  else
+    return false
+  end
+
+end
+
+def position_taken?(board, index)
+
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  else
+    return true
+  end
+
+end
