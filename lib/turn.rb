b3310576b62816af1a0adcafc13273177603d6b4
Alex Ines <lex@blackcat.watch>
2020-05-03 01:58:11 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  user_choice = input_to_index(gets.strip)
+
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && (board[index] == " " || board[index] == "")
+    true
+  else
+    false
+  end
+end
+
+def move(board, index, token="X")
+  if valid_move?(board, index)
+    board[index] = token
+  end
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
